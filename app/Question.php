fcc06d3b45dd6318944b2c561594cd4f8f417b0b
DELL <tarun.kachhela2000@gmail.com>
2020-04-08 02:15:03 +0530

    answer model migration

--- app/Question.php
+++ app/Question.php
@@ -31,5 +31,14 @@ class Question extends BaseModel
         }
         return "unanswered";
     }
+    public function answers(){
+        return $this->hasMany(Answer::class);
+    }
+
+    //Mutator, kyu use karre kyuki meko koi toh event chahye like apne case me title tha and slug is related to slug title: 'hi lraveel' slug 'hi-lraveel' iseleye relate karle	   
+
+
+//    The syntax is as followed abhi hamne isko call kiya iseleye apne toh apni responsibility hai dalne ki title ko	
+
 
 }
