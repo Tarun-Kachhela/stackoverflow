d0269e8f53d32d90066205112d87d899d03077af
DELL <tarun.kachhela2000@gmail.com>
2020-04-09 02:44:05 +0530

    answer of any question can edit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Providers;
+use App\Answer;
+use App\Policies\AnswerPolicy;
 use App\Policies\QuestionPolicy;
 use App\Question;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -15,7 +17,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
-        Question::class => QuestionPolicy::class
+        Question::class => QuestionPolicy::class,
+        Answer::class => AnswerPolicy::class,
     ];
 
     /**
